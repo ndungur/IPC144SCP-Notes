65b0813bb1d3770fdb6744e7c63d90d3003be0df
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2019-01-16 14:56:06 -0500

    first C program in class

---
+++ ClassNotes/01-Jan16/prg.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+  printf("what you write will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you write\n will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you write will be printed.\n");
+  printf("what you wr\tite will be printed.\n");
+  return 0;
+}
+
+
+/*
+type  myfunction(type ....){
+    whatever;
+    whatever;
+    
+
+    return somethig;
+}
+*/
