5323275fb45a05653a17487eec3313d4d2f692b1
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2019-01-27 23:57:47 -0500

    workshop 2 solution

---
+++ Workshops/WS02/at_home/cashRegister.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+
+
+
+
+
+  return 0;
+}
+
+/*
+Please enter the amount due: $9.03
+Amount due after $1.17 tax is: $10.20
+Enter the total cash received from the customer: $20
+Total change due back to the customer: $9.80
+Toonies required: 4, change owing $1.80
+Loonies required: 1, change owing $0.80
+Quarters required: 3, change owing $0.05
+Dimes required: 0, change owing $0.05
+Dimes required: 1
+Pennies required: 0
+
+*/
---
+++ Workshops/WS02/in_lab/cashRegister.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+
+
+
+  return 0;
+}
+
+/*
+Please enter the amount due: $9.03
+Amount due after $1.17 tax is: $10.20
+Enter the total cash received from the customer: $20
+Total change due back to the customer: $9.80
+Toonies required: 4, change owing $1.79
+Loonies required: 1, change owing $0.79
+
+*/
