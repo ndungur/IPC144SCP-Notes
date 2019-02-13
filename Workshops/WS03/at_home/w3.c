61ae88cb5e9b3134589215d069c5da368864ea55
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2019-02-13 17:54:03 -0500

    Workshop 3

---
+++ Workshops/WS03/at_home/w3.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main() {
+  /* variable declaration */
+
+  /* code for printing the first line */
+
+  /* code for printing the second line */
+
+  /* code for printing the third line */
+
+  /* code for printing the fourth line */
+
+  /* code for printing the fifth line */
+
+  return 0;
+}
