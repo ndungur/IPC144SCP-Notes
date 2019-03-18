27c11b557cf487604de35057989cac5a358acf78
Fardad Soleimanloo <fardad.soleimanloo@senecacollege.ca>
2019-03-18 11:12:46 -0400

    ms2 tester

---
+++ Project/MS1/144Proj/ms2tester.c
@@ -0,0 +1,16 @@
+
+// Final Project Milestone 2
+// Student defined values tester
+// Version 1.0
+// Date	2019/03/11
+// Author	Fardad Soleimanloo
+// Description
+// This program test the student implementation of the UI tools
+//
+/////////////////////////////////////////////////////////////////
+
+#include "grapp.h"
+int main(void) {
+  GroceryInventorySystem();
+  return 0;
+}
